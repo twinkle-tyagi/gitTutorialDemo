1ff00767fd24addd8152e6a6d00d6a4b79e7375d
twinkle-tyagi <76639382+twinkle-tyagi@users.noreply.github.com>
2022-04-05 15:57:48 +0530

    DOM task 4

---
+++ main.js
@@ -0,0 +1,54 @@
+const butt = document.querySelector('.btn');
+
+butt.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('.container').style.background = 'yellow';
+    document.querySelector('#my-form').style.background = 'pink';
+})
+
+butt.addEventListener('mouseover',(e) => {
+    e.preventDefault();
+    document.querySelector('.btn').style.background = 'blue';
+    document.querySelector('#my-form').style.background = 'yellow';
+})
+
+butt.addEventListener('mouseout', function(e){
+    e.preventDefault();
+    butt.style.background = 'yellow';
+    document.querySelector('#my-form').style.background = '#ccc';
+});
+
+const myform = document.querySelector('#my-form');
+const nam = document.querySelector('#name');
+const emails = document.querySelector('#email');
+const msgs = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+
+myform.addEventListener('click', onClick);
+
+function onClick(e)
+{
+    e.preventDefault();
+
+    if(nam.value === '' || emails.value === '')
+    {
+        //alert('fill both');
+        msgs.classList.add('error');
+        msgs.innerHTML = 'please enter both fields';
+
+        setTimeout(() => msgs.remove(),3000);
+    }
+    else
+    {
+        //console.log(`Name is ${nam.value}, email is ${emails.value}`)
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nam.value}, 
+        ${emails.value}`));
+        userList.appendChild(li);
+
+        // clear fields
+        nam.value = '';
+        emails.value='';
+    }
+}
---
+++ main1.js
@@ -0,0 +1,42 @@
+//console.dir(document);
+//console.log(document.domain);
+//console.log(document.URL);
+//console.log(document.title);
+//console.log(document.head);
+//console.log(document.all);
+//document.all[10].textContent = "hello";
+//console.log(document.forms);
+//console.log(document.links);
+//console.log(document.forms[0]);
+//console.log(document.images);
+
+// Get ELEMENT by ID
+
+//console.log(document.getElementById('header'));
+//let heade = document.getElementById('header');
+//let header1 = document.getElementById('main-header');
+//console.log(heade);
+//heade.textContent = 'Hello';
+//heade.innerText = 'Nice Guy';
+//console.log(heade.innerText);
+//console.log(heade.textContent);
+//heade.innerHTML = '<h3> nice man</h3>';
+//header1.style.borderBottom = 'solid 3px #000';
+
+let addItem = document.getElementById('add-user');
+addItem.style.color = 'green';
+addItem.style.fontWeight = 'bold';
+
+// Get ELEMENTS by CLASS NAME
+
+let items = document.getElementsByClassName('item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'glass';
+items[2].style.backgroundColor = "yellow";
+//items[0].style.fontWeight = 'bold';
+
+for(let i =0;i<items.length;i++)
+{
+    items[i].style.fontWeight = 'bold';
+}
