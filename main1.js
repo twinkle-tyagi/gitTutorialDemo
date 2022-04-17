d5d14da7e26273aa72c29db9e6a8623d3d978c7d
twinkle-tyagi <76639382+twinkle-tyagi@users.noreply.github.com>
2022-04-17 22:36:30 +0530

    new

--- main1.js
+++ main1.js
@@ -162,26 +162,3 @@ let newli = document.querySelector('ul .item');
 // (position to insert, what to insert);
 newli.insertAdjacentElement('beforebegin', newitem);
 */
-
-let form = document.getElementById('my-form');
-let itemlist = document.getElementById('items');
-
-form.addEventListener('submit', addItems);
-
-function addItems(e)
-{
-    e.preventDefault();
-
-    // get input value
-    let newItem = document.getElementById('name').value;
-    
-    //create new li element
-    let li = document.createElement('li');
-    //add class
-    li.className = 'item';
-    // add text node with input value
-    li.appendChild(document.createTextNode(newItem));
-
-    itemlist.appendChild(li);
-
-}
