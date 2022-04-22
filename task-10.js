ce88fbbad9c21772066b48e76c238c8fd18a24f2
twinkle-tyagi <76639382+twinkle-tyagi@users.noreply.github.com>
2022-04-22 15:33:17 +0530

    task-10

--- task-10.js
+++ task-10.js
@@ -2,7 +2,7 @@ let form = document.getElementById('my-form');
 form.addEventListener('submit', (e)=>
 {
     e.preventDefault();
-    let name1 = document.getElementById('name').value;
-    let email1 = document.getElementById('email').value;
-    localStorage.setItem(name1,email1);
+    let name1 = document.getElementById('name').value; //get name
+    let email1 = document.getElementById('email').value; // get email
+    localStorage.setItem(name1,email1); //store in local storage as key value pair
 });
